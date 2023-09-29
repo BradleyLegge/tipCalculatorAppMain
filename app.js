b77de800ca901c88a07c9791db8809d137b1a1a9
Bradley Legge <bmlegge12@gmail.com>
2023-09-29 16:27:39 -0400

    added start js

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const billInput = document.querySelector('#bill-input')
+const customInput = document.querySelector('.custom')
+const numPeopleInput = document.querySelector('#num-people-input')
 
+const percentFiveBtn = document.querySelector('#five')
+const percentTenBtn = document.querySelector('#ten')
+const percentFifteenBtn = document.querySelector('#fifteen')
+const percentTwentyFiveBtn = document.querySelector('#twenty-five')
+const percentFiftyBtn = document.querySelector('#fifty')
+const resetBtn = document.querySelector('.resetBtn')
 
-window.addEventListener("DOMContentLoaded", (e) => {
-    console.log(billInput)
-})
+const tipAmountPerPerson = document.querySelector('#tip-amount-per-person')
+const tipTotal = document.querySelector('#tip-total')
+
+function validateBillInput(){}
+
+function validatePeopleInput(){}
+
+percentFiveBtn.addEventListener('click', )
+
+function calcFivePercent(){
+    let fivePercentTip = billInput.value * .05
+}
