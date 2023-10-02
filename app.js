9b3ceb84c0619d19f4dab4fe609ffeff3793c2ab
Bradley Legge <bmlegge12@gmail.com>
2023-10-01 20:32:20 -0400

    Added functionality to buttons/input field for tip

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const billInput = document.getElementById('bill-input')
-let customInput = document.querySelector('.custom')
-let numPeopleInput = document.querySelector('#num-people-input')
+const customInput = document.querySelector('.custom')
+const numPeopleInput = document.querySelector('#num-people-input')
 
 const percentFiveBtn = document.getElementById('five')
 const percentTenBtn = document.querySelector('#ten')
@@ -18,33 +18,66 @@ function validatePeopleInput(){}
 
 function validateNumOfPeople(){}
 
-percentFiveBtn.addEventListener('click', calcFivePercent)
+
 resetBtn.addEventListener('click', resetForm)
 
+percentFiveBtn.addEventListener('click', calcFivePercent)
 function calcFivePercent(){
     let fivePercentTip = billInput.value * .05
-    console.log(fivePercentTip)
-    calcTotalTip(fivePercentTip)
+    let fivePercentTipPerPerson = fivePercentTip / numPeopleInput.value
+    tipAmountPerPerson.innerHTML = `$${fivePercentTipPerPerson.toFixed(2)}`
+    calcTotalPerPerson(fivePercentTipPerPerson.toFixed(2))
+}
+
+percentTenBtn.addEventListener('click', calcTenPercent)
+function calcTenPercent(){
+    let tenPercentTip = billInput.value * .10
+    let tenPercentTipPerPerson = tenPercentTip / numPeopleInput.value
+    tipAmountPerPerson.innerHTML = `$${tenPercentTipPerPerson.toFixed(2)}`
+    calcTotalPerPerson(tenPercentTipPerPerson.toFixed(2))
 }
 
 percentFifteenBtn.addEventListener('click', calcFifteenPercent)
-
 function calcFifteenPercent(){
     let fifteenPercentTip = billInput.value * .15
-    console.log(fifteenPercentTip)
     let fifteenPercentTipPerPerson = fifteenPercentTip / numPeopleInput.value
     tipAmountPerPerson.innerHTML = `$${fifteenPercentTipPerPerson.toFixed(2)}`
-    console.log(fifteenPercentTipPerPerson)
     calcTotalPerPerson(fifteenPercentTipPerPerson.toFixed(2))
+}
+
+percentTwentyFiveBtn.addEventListener('click', calcTwentyFivePercent)
+function calcTwentyFivePercent(){
+    let twentyFivePercentTip = billInput.value * .25
+    let twentyFivePercentTipPerPerson = twentyFivePercentTip / numPeopleInput.value
+    tipAmountPerPerson.innerHTML = `$${twentyFivePercentTipPerPerson.toFixed(2)}`
+    calcTotalPerPerson(twentyFivePercentTipPerPerson.toFixed(2))
+}
+
+percentFiftyBtn.addEventListener('click', calcFiftyPercent)
+function calcFiftyPercent(){
+    let fiftyPercentTip = billInput.value * .50
+    let fiftyPercentTipPerPerson = fiftyPercentTip / numPeopleInput.value
+    tipAmountPerPerson.innerHTML = `$${fiftyPercentTipPerPerson.toFixed(2)}`
+    calcTotalPerPerson(fiftyPercentTipPerPerson.toFixed(2))
+}
+
+customInput.addEventListener('input', calcCustomPercent)
+function calcCustomPercent(){
+    let customePercentTip = billInput.value * (customInput.value / 100)
+    let customPercentTipPerPerson = customePercentTip / numPeopleInput.value
+    tipAmountPerPerson.innerHTML = `$${customPercentTipPerPerson.toFixed(2)}`
+    calcTotalPerPerson(customPercentTipPerPerson.toFixed(2))
 }
 
 function calcTotalPerPerson(tip){
     let totalAmount = (billInput.value/numPeopleInput.value) + Number(tip)
-    tipTotal.innerHTML = `$${totalAmount}`
+    tipTotal.innerHTML = `$${totalAmount.toFixed(2)}`
 }
 
 function resetForm(){
-    billInput = ''
-    customInput = ''
-    numPeopleInput = ''
+    billInput.value = ''
+    customInput.value = ''
+    numPeopleInput.value = ''
+    tipAmountPerPerson.innerHTML = `$0.00`
+    tipTotal.innerHTML = `$0.00`
 }
